3dfb61c17dd37c3fe5eb422c6af62b4e099e788e
Valentindg2506 <valentindegennaro@gmail.com>
2025-12-13 14:33:42 +0100

    1

--- back/BD.sql
+++ back/BD.sql
@@ -1,22 +1,21 @@
 CREATE TABLE usuario (
-  id INT,
-  nombre VARCHAR(255),
-  apellidos VARCHAR(255),
-  correo VARCHAR(255),
-  contrasea VARCHAR(255),
-  PRIMARY KEY (id)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(255),
+	apellidos VARCHAR(255),
+	usuario VARCHAR(255),
+	correo VARCHAR(255),
+	contrasena VARCHAR(255)
 );
 
 CREATE TABLE contenido (
-  id INT,
-  usuario_id INT,
-  titulo VARCHAR(255),
-  tipo ENUM('pelicula', 'serie'),
-  estado ENUM('vista', 'por_ver', 'viendo') NOT NULL DEFAULT 'por_ver',
-  puntuacion VARCHAR(255),
-  comentario VARCHAR(255),
-  fecha_visualizacion VARCHAR(255),
-  nivel_prioridad VARCHAR(10),
-  PRIMARY KEY (id),
-  CONSTRAINT fk_contenido_1 FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	usuario_id INT,
+	titulo VARCHAR(255),
+	tipo ENUM('pelicula', 'serie'),
+	estado ENUM('Por_ver', 'Viendo', 'Vistas') NOT NULL DEFAULT 'Por_ver',
+	puntuacion VARCHAR(255),
+	comentario VARCHAR(255),
+	fecha_visualizacion VARCHAR(255),
+	nivel_prioridad ENUM('Alta', 'Media', 'Baja') NOT NULL DEFAULT 'Media',
+	CONSTRAINT fk_contenido_1 FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
---
+++ back/conexion.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+
+	$host = "localhost";
+	$user  = "u908766211_adminviews"; 
+	$pass = "Adminviews2526$";     
+	$db   = "u908766211_adminviews";
+
+	$conexion = new mysqli($host, $user, $pass, $db);
+
+?>
+
+
