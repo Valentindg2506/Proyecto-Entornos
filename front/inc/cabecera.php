2e4ce0c8657e7fe364b6cd54a2af272299b25d9d
Dani <danieloliveiravidal12@gmail.com>
2025-12-13 13:37:53 +0100

    13/12/2025

---
+++ front/inc/cabecera.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <title>AdminViews</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+    <body>
+        <header>
+            <h1>AdminViews</h1>
+        </header>
+        <main>
+            
