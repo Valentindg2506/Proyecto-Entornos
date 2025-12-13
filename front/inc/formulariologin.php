b58c4ec6722c226fc1d089065385d706d502a8bb
Dani <danieloliveiravidal12@gmail.com>
2025-12-13 14:59:06 +0100

    13/12/2025

---
+++ front/inc/formulariologin.php
@@ -0,0 +1,10 @@
+<form action="../login.php" method="POST">
+    <div class="controlsesion">
+        <label for="usuario">Nombre usuario</label>
+        <input type="text" name="usuario" id="usuario">
+    </div>
+    <div class="controlsesion">
+        <label for="contrasena">Contraseña</label>
+        <input type="password" name="contrasena" id="contrasena">
+    </div>
+</form
--- front/login.php
+++ front/login.php
@@ -1,14 +1,6 @@
-<?php include "inc/cabera.php"; ?>
-<section id="login">
-    <form>
-        <div class="controlsesion">
-            <label for="usuario">Nombre usuario</label>
-            <input type="text" name="usuario" id="usuario">
-        </div>
-        <div class="controlsesion">
-            <label for="contrasena">Contraseña</label>
-            <input type="password" name="contrasena" id="contrasena">
-        </div>
-    </form
-</section>
-<?php include "inc/piedepagina"; ?>
+<?php
+    
+    $usuario = $_POST['usuario']
+    $contrasena = $_POST['contrasena']
+
+    
